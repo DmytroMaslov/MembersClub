c09b1f7acb2234a1d9176470b6a0138c7cdcc1cb
DmytroMaslov <dmytromaslov@github.com>
2021-12-20 22:07:16 +0200

    Add test for domain

--- domain/member_dao_test.go
+++ domain/member_dao_test.go
@@ -1,21 +1,54 @@
 package domain
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+	"time"
 
-func Test_SaveMember(t *testing.T) {
-	mem1 := Member{
-		Name:  "Dima",
-		Email: "123",
-		//RegistationDate: 123,
+	"github.com/stretchr/testify/assert"
+)
+
+//насколько уместно использовать переприсваение map?
+func Test_SaveMember_succes(t *testing.T) {
+	inputMember := &Member{
+		Name:            "Test",
+		Email:           "test@test.com",
+		RegistationDate: time.Now(),
 	}
+	outMember, err := SaveMember(inputMember)
+	assert.Equal(t, inputMember, outMember, "input and output Member not equal")
+	assert.Nil(t, err, "error not nil")
+	memberInMemory := atomicMembers.members[inputMember.Email]
+	assert.Equal(t, inputMember, memberInMemory, "input member and member in memmory not same")
+}
 
-	mem2 := Member{
-		Name:  "NeDima",
-		Email: "123",
-		//RegistationDate: 123,
+func Test_SaveMember_fail(t *testing.T) {
+	inputMember := &Member{
+		Name:            "Test",
+		Email:           "test@test.com",
+		RegistationDate: time.Now(),
 	}
+	SaveMember(inputMember)
+	_, err := SaveMember(inputMember)
+	assert.NotNil(t, err, "Expect error if try save Member with existing email")
+}
 
-	SaveMember(&mem1)
-	SaveMember(&mem2)
-	t.Error(GetAllMembers())
+func Test_GetAllMembers(t *testing.T) {
+	inputMemberFirst := &Member{
+		Name:            "First",
+		Email:           "first@test.com",
+		RegistationDate: time.Now(),
+	}
+	inputMemberSecond := &Member{
+		Name:            "Second",
+		Email:           "second@test.com",
+		RegistationDate: time.Now(),
+	}
+	inputMembers := []Member{*inputMemberFirst, *inputMemberSecond}
+	atomicMembers = nil
+	atomicMembers = initMembers()
+	SaveMember(inputMemberFirst)
+	SaveMember(inputMemberSecond)
+	outMembers := GetAllMembers()
+	assert.True(t, reflect.DeepEqual(inputMembers, outMembers))
 }
