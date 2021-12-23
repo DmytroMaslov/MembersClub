c62f6e3e3d6c4733dc753f21785f03791b82c378
DmytroMaslov <dmytromaslov@github.com>
2021-12-23 09:47:28 +0200

    heroku app

--- app/app.go
+++ app/app.go
@@ -2,14 +2,24 @@ package app
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/DmytroMaslov/memberclub/controllers"
+	"github.com/DmytroMaslov/memberclub/log"
 )
 
+var port = os.Getenv("PORT")
+
 func StartApp() {
+	if port == "" {
+		log.Get().Error("$PORT must be set")
+		panic("$PORT must be set")
+	}
+	port = ":" + port
+	log.Get().Info("Service run on port:%v", port)
 	http.HandleFunc("/addMember", controllers.AddUser)
 	http.HandleFunc("/getAllMember", controllers.GetAllMember)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DmytroMaslov/memberclub
 go 1.17
 
 require (
+	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.19.1
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
@@ -13,7 +14,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
